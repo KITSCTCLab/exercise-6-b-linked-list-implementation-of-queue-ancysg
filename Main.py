d698cde45b2d067849dc7363070e13c2de253fa8
ancysg <109953797+ancysg@users.noreply.github.com>
2022-09-09 00:13:33 +0530

    3commit

--- Main.py
+++ Main.py
@@ -4,49 +4,49 @@ class Node:
     self.next = None
 
 
-class Stack:
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
+  def enqueue(self, data) -> None:
     # Write your code here
-    new_node = Node(data)
-    new_node.next = self.head
-    self.head = new_node
-
-  def pop(self) -> None:
-    # Write your code here
-    if(self.head != None): 
-      temp = self.head
-      self.head = temp.next
+    temp = Node(data)
+    if self.head == None:
+      self.head = temp
+      self.last = temp
       temp.next = None
-
-  def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here 
-    if(self.head != None):
-      temp = self.head
-      while(temp.next != None):
-        print(temp.data, end = "=>")
-        temp = temp.next
-      else:
-        print(temp.data,"None", sep="=>")
     else:
-      print("None")
+      self.last.next = temp
+      temp.next = None
+      self.last = temp
+
+  def dequeue(self) -> None:
+    # Write your code here
+    if self.head == None:
+      pass
+    else:
+      self.head = self.head.next
+
+  def status(self) -> None:
+    # Write your code here
+    temp = self.head
+    while temp!= None:
+      print(temp.data, "=>", sep="", end = "")
+      temp = temp.next
+    print("None")
 
 
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status() 
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
